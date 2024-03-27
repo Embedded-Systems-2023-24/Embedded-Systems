ec62de9d684a001758ef7e594c39049711750faa
Giannis Athanasiadis <128140023+gianath03@users.noreply.github.com>
2024-03-27 14:52:21 +0200

    Step4: First upload

--- lab1/step4/verilog_input/fpadd_system.v
+++ lab1/step4/verilog_input/fpadd_system.v
@@ -13,16 +13,30 @@
    wire [31:0] fp_out;
      
    // Try this addition of FP numbers 
-   // 6b64b235 + 6ac49214 = 6ba37d9f 
+   // 6b64b235 + 6ac49214 = 6ba37d
+   
    
    // Instantiate the FP adder 
+   fpadd_pipelined fpadd_pipelined_inst (clk,
+                                         rst,
+                                         32'h6b64b235, 
+                                         32'h6ac49214,  
+		     		                          fp_out);
         
    assign leds = fp_out[7:0];
    
    // Instantiate the 7segment display output 0 
-  
+   SSDisplays_module SSDisplays_inst0 (clk,
+                                       rst,
+                                       fp_out[31:24],
+                                       {a0, b0, c0, d0, e0, f0, g0},
+                                       an0);
    
    // Instantiate the 7segment display output 1
-
+   SSDisplays_module SSDisplays_inst1 (clk,
+                                       rst,
+                                       fp_out[23:16],
+                                       {a1, b1, c1, d1, e1, f1, g1},
+                                       an1);
 
 endmodule
